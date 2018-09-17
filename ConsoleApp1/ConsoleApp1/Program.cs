e6dd21c509a89de0a255bc75fb4e67cbced6156e
August Ramos <AugustRamos@stfrancis.edu>
2018-09-17 18:06:33 -0500

    Created program to find waldo withing array of strings.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Tom", "Bob", "Dana", "Julie", "Sarah", "Fred", "Waldo", "Jenny", "Cathy" };
+            int position = 0;
+            bool found = false;
+            int max = names.Length;
+
+
+            for (int t = 0; t < max; t++)
+            {
+                if (names[t] == "Waldo")
+                {
+                    found = true;
+                    break;
+                }
+                else
+                {
+                    position++;
+                }
+            }
+            Console.WriteLine($"Waldo has been found at {position}.");
+        }
+
+           
+        
+    }
+}
